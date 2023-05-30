d260fe765bd0eea492e410b2897d6ecd05f38e72
callme-rojas <leowfr.flrq@gmail.com>
2023-05-30 00:21:26 -0400

    modified:   CLOUD/dp.php

--- CLOUD/dp.php
+++ CLOUD/dp.php
@@ -2,7 +2,7 @@
 class Database {
     private $host = "localhost";
     private $db_name = "relojeria";
-    private $username = "franco";
+    private $username = "phpmyadmin";
     private $password = "12345";
     private $conn;
 
